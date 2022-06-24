a3afb0e61ead3d1a04e86320355d362b838c0c5c
AlexRios <riosalexandra91@gmail.com>
2022-06-24 17:31:43 -0500

    created directory for edabit practice/began solutions

---
+++ edabit-practice/edabit-JS-practice.js
@@ -0,0 +1,21 @@
+(function(){
+    "use strict";
+
+// create a function that takes 2 numbers as arguments and returns their sum
+   function sum(num1, num2){
+       return (num1 + num2);
+   }
+
+//   create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers
+    function nextEdge(side1, side2){
+       return (side1 + side2) -1;
+    }
+
+//    create a function that
+
+
+
+
+
+
+})();
